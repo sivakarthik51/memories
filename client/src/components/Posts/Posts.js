40b401e5ac1c0f1d6853f6c07abd5c32e3d76445
Siva Karthik <siramesh@akamai.com>
2021-01-18 21:57:02 +0530

    Initial Commit

---
+++ client/src/components/Posts/Posts.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Post from './Post/Post';
+import {useSelector} from 'react-redux';
+import useStyles from './styles';
+import { CircularProgress, Grid } from '@material-ui/core';
+
+const Posts = ({ setCurrentId }) => {
+    const classes = useStyles();
+    const posts = useSelector((state) => state.posts); 
+    return (
+        !posts.length? <CircularProgress />: (
+            <Grid className={classes.container} container alignItems="stretch" spacing={3}>
+                {posts.map((post) => (
+                    <Grid key={post._id} item xs={12} sm={6}>
+                        <Post post={post} setCurrentId={setCurrentId} />
+
+                    </Grid>
+                ))}
+
+            </Grid>
+        )
+    );
+}
+
+export default Posts;
---
+++ server/index.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import postRoutes from './routes/posts.js';
+
+const app = express();
+dotenv.config();
+app.use(cors());
+
+
+app.use(bodyParser.json({ limit: '30mb', extended:true}));
+app.use(bodyParser.urlencoded({ limit: '30mb', extended:true}));
+
+app.use('/posts',postRoutes);
+
+const CONNECTION_URL= process.env.CONNECTION_URL;
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser:true, useUnifiedTopology:true, useFindAndModify: false})
+    .then(() => app.listen(PORT, () => console.log(`Server Running on port ${PORT}`)))
+    .catch((error) => console.error(error.message));
